d543c38b6b8f8f313da1ad04f30efdf35bac7530
Zelanious <71904533+Zelanious@users.noreply.github.com>
2022-12-03 22:12:28 +0000

    Initial commit

---
+++ Source/RBM_Minotaur_Mod/Class1.cs
@@ -0,0 +1,103 @@
+﻿using Verse;
+using RimWorld;
+using Verse.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+
+namespace Rimworld
+{
+    [DefOf]
+    public static class DamageDefOf
+    {
+        public static DamageDef Terrified;
+    }
+
+    /*
+[DefOf]
+public static class HediffDefOf
+{
+    public static HediffDef HeDiffTerrified;
+}
+
+
+public class JobGiver_FleeSeeingRed : ThinkNode_JobGiver
+{
+
+    protected override Job TryGiveJob(Pawn pawn)
+    {
+        for (int index = 0; index < this.hediffs.Count; index++)
+        {
+            if (pawn.health.hediffSet.HasHediff())
+            {
+                HediffStage curStage = this.hediffs[index].CurStage;
+                if (curStage != null)
+                {
+                    a *= curStage.hungerRateFactor;
+                }
+            }
+        }
+        {
+            return null;
+        }
+        pawn.jobs.StartJob(flee)
+
+        Thing enragedPawn = pawn.mindState.knownExploder;
+        if ((float)(pawn.Position - knownExploder.Position).LengthHorizontalSquared > 81f)
+        {
+            return null;
+        }
+        IntVec3 result;
+        if (!RCellFinder.TryFindDirectFleeDestination(knownExploder.Position, 9f, pawn, out result))
+        {
+            return null;
+        }
+        Job job = JobMaker.MakeJob(JobDefOf.Goto, result);
+        job.locomotionUrgency = LocomotionUrgency.Sprint;
+        return job;
+    }
+
+    public const float FleeDist = 9f;
+    */
+
+    public class CompAbilityEffect_Terrify : CompAbilityEffect
+    {
+        public new CompProperties_AbilityTerrify Props
+        {
+            get
+            {
+                return (CompProperties_AbilityTerrify)this.props;
+            }
+        }
+        public bool ShouldHaveInspectString
+        {
+            get
+            {
+                return ModsConfig.BiotechActive;
+            }
+        }
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            GenExplosion.DoExplosion(target.Cell, this.parent.pawn.MapHeld, this.Props.terrorRadius, DamageDefOf.Terrified, null, -1, -1f, null, null, null, null, null, 0f, 1, null, false, null, 0f, 1, 0f, false, null, null, null, true, 1f, 0f, true, null, 1f);
+        }
+
+        public override void DrawEffectPreview(LocalTargetInfo target)
+        {
+            GenDraw.DrawRadiusRing(target.Cell, this.Props.terrorRadius);
+        }
+    }
+
+
+    public class CompProperties_AbilityTerrify : CompProperties_AbilityEffect
+    {
+        public CompProperties_AbilityTerrify()
+        {
+            this.compClass = typeof(CompAbilityEffect_Terrify);
+        }
+        public float terrorRadius;
+    }
+}
---
+++ Source/RBM_Minotaur_Mod/Class2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace RBM_Minotaur_Mod
+
+{
+    // Token: 0x02000F8B RID: 3979
+    public class CompProperties_AbilityPetrify : CompProperties_AbilityEffect
+    {
+        // Token: 0x06005D85 RID: 23941 RVA: 0x001FBF24 File Offset: 0x001FA124
+        public CompProperties_AbilityPetrify()
+        {
+            this.compClass = typeof(CompAbilityEffect_Smokepop);
+        }
+
+        // Token: 0x040038F1 RID: 14577
+        public float smokeRadius;
+    }
+}
+
+{
+    // Token: 0x02000F8C RID: 3980
+    public class CompAbilityEffect_Petrify : CompAbilityEffect
+    {
+        // Token: 0x17001011 RID: 4113
+        // (get) Token: 0x06005D86 RID: 23942 RVA: 0x001FBF3C File Offset: 0x001FA13C
+        public new CompProperties_AbilityPetrify Props
+        {
+            get
+            {
+                return (CompProperties_AbilityPetrify)this.props;
+            }
+        }
+
+
+        // Token: 0x06005D88 RID: 23944 RVA: 0x001FBF6C File Offset: 0x001FA16C
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            GenExplosion.DoExplosion(target.Cell, this.parent.pawn.MapHeld, this.Props.smokeRadius, DamageDefOf.Smoke, null, -1, -1f, null, null, null, null, null, 0f, 1, new GasType?(GasType.BlindSmoke), false, null, 0f, 1, 0f, false, null, null, null, true, 1f, 0f, true, null, 1f);
+        }
+
+        // Token: 0x06005D89 RID: 23945 RVA: 0x001FBFF8 File Offset: 0x001FA1F8
+        public override void DrawEffectPreview(LocalTargetInfo target)
+        {
+            GenDraw.DrawRadiusRing(target.Cell, this.Props.smokeRadius);
+        }
+    }
+}
+}
