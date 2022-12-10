64161f2a468fca67cd4a1a1807ff85414a3576e2
Zelanious <71904533+Zelanious@users.noreply.github.com>
2022-12-10 22:44:50 +0000

    Split and renamed project files

--- Source/RBM_Minotaur_Mod/MidasTouch.cs
+++ Source/RBM_Minotaur_Mod/MidasTouch.cs
@@ -6,50 +6,6 @@ using Verse.Sound;
 
 namespace Rimworld
 {
-    [DefOf]
-    public static class DamageDefOf
-    {
-        public static DamageDef Terrified;
-    }
-
-    public class CompAbilityEffect_Terrify : CompAbilityEffect
-    {
-        public new CompProperties_AbilityTerrify Props
-        {
-            get
-            {
-                return (CompProperties_AbilityTerrify)this.props;
-            }
-        }
-        public bool ShouldHaveInspectString
-        {
-            get
-            {
-                return ModsConfig.BiotechActive;
-            }
-        }
-        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
-        {
-            base.Apply(target, dest);
-            GenExplosion.DoExplosion(target.Cell, this.parent.pawn.MapHeld, this.Props.terrorRadius, DamageDefOf.Terrified, null, -1, -1f, null, null, null, null, null, 0f, 1, null, false, null, 0f, 1, 0f, false, null, null, null, true, 1f, 0f, true, null, 1f);
-        }
-
-        public override void DrawEffectPreview(LocalTargetInfo target)
-        {
-            GenDraw.DrawRadiusRing(target.Cell, this.Props.terrorRadius);
-        }
-    }
-
-
-    public class CompProperties_AbilityTerrify : CompProperties_AbilityEffect
-    {
-        public CompProperties_AbilityTerrify()
-        {
-            this.compClass = typeof(CompAbilityEffect_Terrify);
-        }
-        public float terrorRadius;
-    }
-
     //Midas Touch Death Effect
 
     public class HediffCompProperties_MidasTouch : HediffCompProperties
@@ -160,8 +116,6 @@ namespace Rimworld
             //Pawn.Ideo?.RecachePrecepts();
             Pawn.Ideo?.Notify_MemberCorpseDestroyed(Pawn);
 
-
-            
         }
 
     }
