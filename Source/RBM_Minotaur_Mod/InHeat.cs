0e708964617ba44e534515dc27d2afacec6c6089
Zelanious <71904533+Zelanious@users.noreply.github.com>
2022-12-18 19:24:42 +0000

    first in heat conditional hediff pusher test

--- Source/RBM_Minotaur_Mod/InHeat.cs
+++ Source/RBM_Minotaur_Mod/InHeat.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using Verse;
+using Verse.AI;
 using HarmonyLib;
 using RimWorld;
+
 
 
 namespace RBM_Minotaur_Mod
@@ -45,18 +47,56 @@ namespace RBM_Minotaur_Mod
     public static class RBM_HediffDefOf
     {
         public static HediffDef EstrousHeat;
+    }
 
+    [DefOf]
+    public static class RBM_GeneDefOf
+    {
+        public static GeneDef RBM_EstrousCycle;
+    }
+
+    public class ThinkNode_ConditionalPawnInHeatSeason : ThinkNode_Conditional
+    {
+        protected override bool Satisfied(Pawn pawn)                                //Thinknode is satifified if the pawn:
+        {
+            bool geneFlag = false, seasonFlag = false, hediffFlag = true;
+            if (pawn.RaceProps.Humanlike == true)
+            {
+                if (pawn.genes.HasGene(RBM_GeneDefOf.RBM_EstrousCycle) == true)
+                {
+                    geneFlag = true;
+                    if ((GenLocalDate.Season(pawn.Tile) == Season.Spring))
+                    {
+                        seasonFlag = true;
+                        if (!(pawn.health.hediffSet.HasHediff(RBM_HediffDefOf.EstrousHeat)))
+                        {
+                            hediffFlag = false;
+                        }
+                    }
+                    
+                }
+
+            Log.Message("Humanlike Pawn Checked. Has gene: " + geneFlag.ToString() + " is correct season: " + seasonFlag.ToString() + " is already in heat: " + hediffFlag.ToString());
+            }
+            return (                                                                 
+                pawn.RaceProps.Humanlike ||                                        //is not human
+                pawn.genes.HasGene(RBM_GeneDefOf.RBM_EstrousCycle) ||              //Does not have the Estrous Cycle Gene
+                (GenLocalDate.Season(pawn.Tile) == Season.Spring) ||               //is not on a tile where the season is currently Spring
+                !(pawn.health.hediffSet.HasHediff(RBM_HediffDefOf.EstrousHeat))      //is already in heat
+            );
+        }
 
     }
 
-    /*
-     
-    public class CompInitialHediff : ThingComp
+    public class JobGiver_PawnInHeatSeason : ThinkNode_JobGiver
     {
-        public override void CompTickRare()
+        protected override Job TryGiveJob(Pawn pawn)
         {
-            base.CompTickRare();
+            Log.Message(pawn.Name + "is in heat.");
+            pawn.health.AddHediff(RBM_HediffDefOf.EstrousHeat);
+            return (Job)null;
         }
     }
-    */
+
 }
+
