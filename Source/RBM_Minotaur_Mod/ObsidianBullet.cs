4e301dd08e5279f3bbadfe4eb8ccc1ddbb8770df
Zelanious <71904533+Zelanious@users.noreply.github.com>
2023-01-02 02:55:19 +0000

    Taurailgun Fear Explosion implementation

--- Source/RBM_Minotaur_Mod/ObsidianBullet.cs
+++ Source/RBM_Minotaur_Mod/ObsidianBullet.cs
@@ -7,16 +7,20 @@ namespace RBM_Minotaur
 {
 
     [StaticConstructorOnStartup]
-    public class Projectile_ObsidianBullet : Projectile
+    public class Projectile_ObsidianBullet : Projectile_Explosive
     {
         protected override void Impact(Thing hitThing, bool blockedByShield = false)
         {
-            Map map = this.launcher.Map;
+
+            Map map = base.Map;
             base.Impact(hitThing);
             IntVec3 position = base.Position;
-            ThingDef def = this.def;
             float explosionRadius = this.def.projectile.explosionRadius;
-            GenExplosion.DoExplosion(position, map, explosionRadius, DamageDefOf.Smoke, null, -1, -1f, null, null, null, null, null, 0f, 1, null, false, null, 0f, 1, 0f, false, null, null, null, true, 1f, 0f, true, null, 1f);
+            DamageDef damageDef = this.def.projectile.damageDef;
+            int damageAmount = this.DamageAmount;
+            float armorPenetration = this.ArmorPenetration;
+
+            GenExplosion.DoExplosion(position, map, explosionRadius, damageDef, null, damageAmount, armorPenetration, null, null, null, null, null, 0f, 1, null, false, null, 0f, 1, 0f, false, null, null, null, true, 1f, 0f, true, null, 1f);
             if (map != null)
             {
                 List<Pawn> mapPawns = map.mapPawns.AllPawnsSpawned;
