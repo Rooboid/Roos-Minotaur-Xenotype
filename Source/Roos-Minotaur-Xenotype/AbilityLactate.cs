fc7b3ce5702ffa0f0b8d39f4ccb06738f3c8878d
Zelanious <71904533+Zelanious@users.noreply.github.com>
2024-09-27 15:48:30 +0100

    fixed underage milkies

--- Source/Roos-Minotaur-Xenotype/AbilityLactate.cs
+++ Source/Roos-Minotaur-Xenotype/AbilityLactate.cs
@@ -1,5 +1,6 @@
 ﻿using RimWorld;
 using Verse;
+using static RimWorld.PsychicRitualRoleDef;
 
 namespace RBM_Minotaur
 {
@@ -32,7 +33,7 @@ namespace RBM_Minotaur
         {
             //if (MinotaurSettings.debugMessages) { Log.Message("RBM Is Running: (AbilityLactate) public override bool GizmoDisabled(out string reason)"); }
 
-            if (!this.parent.pawn.ageTracker.Adult)
+            if (!parent.pawn.ageTracker.Adult || parent.pawn.ageTracker.AgeBiologicalYears < 18 || parent.pawn.ageTracker.CurLifeStage != LifeStageDefOf.HumanlikeAdult)
             {
                 reason = "Pawn is not yet an adult.";
                 return true;
@@ -49,6 +50,12 @@ namespace RBM_Minotaur
         {
             get
             {
+
+                if (!parent.pawn.ageTracker.Adult || parent.pawn.ageTracker.AgeBiologicalYears < 18 || parent.pawn.ageTracker.CurLifeStage != LifeStageDefOf.HumanlikeAdult)
+                {
+                    return true;
+                }
+
                 if (this.parent.pawn.gender == Gender.Male && !MinotaurSettings.milkableMales)
                 {
                     return true;
@@ -58,6 +65,7 @@ namespace RBM_Minotaur
                 {
                     return true;
                 }
+
                 return false;
             }
         }
